40aedfdf3efa2bb9f21606447b30b23ee1d6a669
Chris Blumentritt <cblument@gmail.com>
2014-12-30 15:45:00 -0600

    Making class and function names follow pip

--- spotternetwork.py
+++ spotternetwork.py
@@ -1,19 +1,19 @@
 import feedparser
 import re
 
-class spotternetwork:
+class SpotterNetwork:
   def __init__(self):
     self.locations = []
 
-  def addLocation(self, name, uid, location_datetime, latitude, longitude):
+  def add_location(self, name, uid, location_datetime, latitude, longitude):
     in_hash = { 'name': name, 'uid': uid, 'time': location_datetime, \
                 'latitude': latitude, 'longitude': longitude }
     self.locations.append(in_hash)
 
-  def getLocations(self):
+  def get_locations(self):
     return self.locations
 
-  def loadFromUrl(self, url = 'http://www.spotternetwork.org/feeds/rss-positions.xml'):
+  def load_entries(self, url = 'http://www.spotternetwork.org/feeds/rss-positions.xml'):
     feed = feedparser.parse(url)
     p_name = re.compile('\(Name\)([^\(]*)')
     p_email = re.compile('\(Name\)([^\(]*)')
@@ -25,13 +25,13 @@ class spotternetwork:
       location_time = p_time.search(e.summary).group(1).strip()
       self.addLocation(name, uid, location_time, e.geo_lat, e.geo_long)
 
-  def printLocations(self):
+  def print_locations(self):
     for r in self.locations:
       print "Name: {}, ID: {}, Location: {},{}, Time: {}".format( \
           r['name'], r['uid'], r['latitude'], r['longitude'], \
           r['time'])
 
 if __name__ == "__main__":
-  locations = spotternetwork()
-  locations.loadFromUrl()
-  locations.printLocations()
+  locations = SpotterNetwork()
+  locations.load_entries()
+  locations.print_locations()
